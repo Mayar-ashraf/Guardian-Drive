62d071e822260e1d4287d2f18355569edd32db7e
Mayar-ashraf <mayarashraf2002@gmail.com>
2026-04-08 01:59:48 +0200

    solved form data problem occured when sending request by flutter app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 import express from "express"
 import authRoute from "./routes/authRoute.js"
 const app = express();
-app.use(express.json())
-app.use("/api/auth", authRoute)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoute);
 app.listen(3000, () => {
     console.log("server is running on http://localhost:3000")
 })
