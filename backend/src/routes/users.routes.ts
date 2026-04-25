eb37236a356398b623526ef15fc86d4a7d01d818
Rowane Gamal <rowanegamal18@gmail.com>
2026-04-25 22:17:36 +0300

    users CRUD done

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { get } from "node:http";
-import { getAllusers, getuserbyID } from "../controllers/users.controller";
+import { get} from "node:http";
+import { deleteuserbyID, edituserbyID, getAllusers, getuserbyID } from "../controllers/users.controller";
 import { authorize ,authenticate} from './../middleware/AuthMiddleware';
 import { Role } from "../../generated/prisma/enums";
 
@@ -8,5 +8,8 @@ const router = express.Router();
 
 router.get("/getAllusers",authenticate,authorize(Role.ADMIN,Role.FLEET_MANAGER),getAllusers);
 router.get("/getuserbyID/:id",authenticate,authorize(Role.ADMIN,Role.FLEET_MANAGER,Role.DRIVER),getuserbyID);
+router.put("/edituserbyID/:id",authenticate,authorize(Role.ADMIN,Role.FLEET_MANAGER,Role.DRIVER),edituserbyID);
+router.delete("/deleteuserbyID/:id",authenticate,authorize(Role.ADMIN,Role.FLEET_MANAGER,Role.DRIVER),deleteuserbyID);
+
 
 export default router;
