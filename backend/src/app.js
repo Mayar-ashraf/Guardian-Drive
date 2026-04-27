f1d62e354afdb6f258f9bdbee892bcf8bdd5bedc
Rowane Gamal <rowanegamal18@gmail.com>
2026-04-27 20:20:12 +0300

    resolved conflicts

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,6 @@ import usersRoutes from "./routes/users.routes.ts";
 import alertRoute from "./routes/alert.route.ts";
 import firstAidRoute from "./routes/firstAid.route.ts";
 
-
 const app = express();
 app.use(express.json())
 app.use("/api/auth", authRoute)
@@ -17,11 +16,11 @@ app.use("/api/trips", authMiddleware.authenticate, tripsRoute)
 app.use("/api/password", passwordRoutes)
 app.use("/api/cars",carsRoute)
 app.use("/api/users", firstAid)
+app.use("/api/users",usersRoutes)
 app.use("/api/first-aid-guidance", firstAidRoute)
 app.use(express.static(path.join(process.cwd(), "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/auth", authRoute);
 app.listen(3000, () => {
     console.log("server is running on http://localhost:3000")
 })
