7b2c6f381d6890977dd329b54e604a01a040bae0
Omnia Farouk <omniafarouk2003@gmail.com>
2026-05-04 19:13:53 +0300

    deleted redundant carRoutes file

--- backend/src/schema/alert.ts
+++ backend/src/schema/alert.ts
@@ -76,6 +76,13 @@ export const getAlertByIdSchema = z.object({
     })
 });
 
+export const alertIdSchema = z.object({
+    alertId: z.coerce.number("Alert Id must be a valid number")
+        .int("Alert Id must be an integer")
+        .positive("Alert Id must be positive")
+}).strict();
+
+
 export type CreateAlertInput = z.infer<typeof CreateAlertSchema>;
 export type UpdateAlertInput = z.infer<typeof UpdateAlertSchema>;
 export type FilterAlertInput = z.infer<typeof FilterAlertSchema>;
--- backend/src/schema/firstAidGuidance/getFirstAid.schema.ts
+++ backend/src/schema/firstAidGuidance/getFirstAid.schema.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 import { alertIdSchema } from "../alert";
+import { getAlertById } from "../../controllers/alert.controller";
 
 export const getFirstAidSchema = z.object({
-    params: alertIdSchema,
+    params: getAlertById,
     query: z.object({}).strict()
 });
