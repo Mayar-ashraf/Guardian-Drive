6561b81baef0a76a68103bac9560deba6811e07a
Rowane Gamal <rowanegamal18@gmail.com>
2026-05-12 14:54:36 +0300

    updated password routes & controller

--- backend/src/routes/password.route.ts
+++ backend/src/routes/password.route.ts
@@ -6,4 +6,24 @@ const router = express.Router();
 router.post("/forget-password", forgetPass);
 router.post("/reset-password", resetPass);
 
+
+router.get("/reset-password", (req, res) => {
+  const token = req.query.token;
+
+  const appLink = `guardiandrive://reset-password?token=${token}`;
+
+  res.send(`
+    <html>
+      <body>
+        <script>
+          window.location.href = "${appLink}";
+        </script>
+
+        <p>Redirecting to app...</p>
+      </body>
+    </html>
+  `);
+});
+
+
 export default router;
